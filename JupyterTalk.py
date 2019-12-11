00fa0ce984e6cb1c7370b032d87dfae3516fb90a
Rob Ward <wardrobert@gmail.com>
2019-12-11 17:06:37 -0500

    initial commit

---
+++ JupyterTalk.py
@@ -0,0 +1,66 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Jupyter Notebooks
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # What is a Jupyter Notebook? 
+
+# + [markdown] slideshow={"slide_type": "subslide"}
+# ## This Slide!
+# http://localhost:8888/notebooks/JupyterTalk.ipynb
+
+# + slideshow={"slide_type": "skip"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -
+
+# # Why Jupyter?  
+
+# ## Traditional software development looks like...
+
+
