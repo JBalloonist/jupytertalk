3ccd7e4fe63d262c2ee511cfa123b9d3d380887f
Rob Ward <wardrobert@gmail.com>
2020-02-12 17:14:40 -0500

    updated with new slides

--- JupyterTalk.py
+++ JupyterTalk.py
@@ -13,54 +13,72 @@
 # ---
 
 # + [markdown] slideshow={"slide_type": "slide"}
-# # Jupyter Notebooks
+# # Technology First Data Analytics SIG
+#
+# ## Jupyter Notebooks Talk - February 28, 2020
+#
+# ### Robert Ward - Manager, Advanced Analytics (Speedway)
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Background
+#
+# - Former Accountant (still recovering)
+# - Started learning Python ~7 years ago
+# - LexisNexis
+# - SGS
+# - AIS (Applied Information Sciences)
+# - Speedway
+#
+# **Undergrad in Accounting - Cedarville** <br>
+# **Masters in Business Analytics - American University (Washington DC)**
 
 # + [markdown] slideshow={"slide_type": "slide"}
 # # What is a Jupyter Notebook? 
 
-# + [markdown] slideshow={"slide_type": "subslide"}
-# ## This Slide!
-# http://localhost:8888/notebooks/JupyterTalk.ipynb
+# + [markdown] slideshow={"slide_type": "fragment"}
+# - An interactive development environment
+# - Combines code and output into one page
+# - Development is done through [Project Jupyter](https://jupyter.org/)
+# - Supports over 40 languages (Python, R, Julia, C, etc.)
 
-# + slideshow={"slide_type": "skip"}
+# + [markdown] slideshow={"slide_type": "fragment"}
+# ## Also...
 
+# + [markdown] slideshow={"slide_type": "fragment"}
+#  ## ...this [slideshow](http://localhost:8888/notebooks/JupyterTalk.ipynb)!
 
+# + [markdown] slideshow={"slide_type": "fragment"}
+# ### Thanks to Markdown and Reveal.js
 
+# + [markdown] slideshow={"slide_type": "slide"}
+# # History
 
+# + [markdown] slideshow={"slide_type": "fragment"}
+# - Originated from the IPython project in 2014
+# - IPtyhon was created as an interactive computing environment for Python
+# - Jupyter was created to create a language agnostic interactive environment for data science and scientific computing
 
+# + [markdown] slideshow={"slide_type": "slide"}
+# # Some caveats...
 
+# + [markdown] slideshow={"slide_type": "fragment"}
+# ### There are some strong opinions about notebooks
+#
+# ### Nature [article](https://www.nature.com/articles/d41586-018-07196-1) on Jupyter:
+# ![title](2020-02-11_12-14-13.png)
+#
+# ### Joel Grus'  [I hate notebooks](https://docs.google.com/presentation/d/1n2RlMdmv1p25Xy5thJUhkKGvjtV-dkAIsUXP-AL4ffI/edit#slide=id.g362da58057_0_1) talk at JupyterCon 2018
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# -
-
+# + [markdown] slideshow={"slide_type": "slide"}
 # # Why Jupyter?  
 
+# + [markdown] slideshow={"slide_type": "fragment"}
 # ## Traditional software development looks like...
+#
+# ![title](2019-12-13_10-26-22.png)
+
+# + [markdown] slideshow={"slide_type": "fragment"}
+# # This isn't the way most data analysts or data scientists want to work
+# -
 
 
